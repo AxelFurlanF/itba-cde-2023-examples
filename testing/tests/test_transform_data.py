bd5754f5fc6c8afe1ce49c4325a944e79cb266ed
Axel Furlan <axel.furlan@toptal.com>
2023-09-14 21:06:03 -0400

    adding more tests to show mock and magic mock

--- testing/tests/test_transform_data.py
+++ testing/tests/test_transform_data.py
@@ -1,9 +1,28 @@
 import get_data_from_api as my_module
-from unittest.mock import patch
+from unittest.mock import patch, Mock, MagicMock
+
+
+def test_get_api_data_with_mocks():
+    # Create a mock response object with a .json() method
+    mock_response = MagicMock()
+
+    # Set the .json() method to return a specific dictionary
+    mock_response.json.return_value = {"result_number": "10"}
+
+    # Mock requests.get to return the mock_response
+    my_module.requests.get = Mock(return_value=mock_response)
+
+    # Call the function - instead of calling requests.get(),
+    # it will call the mock, which returns mock_response,
+    # which has a .json() method that returns the test dictionary
+    data = my_module.get_and_transform_data_from_api('http://fakeurl')
+
+    # Assert the function returns the correct data
+    assert data == {"result_number": 10}
 
 
 @patch('get_data_from_api.requests.get')
-def test_get_data_from_api(mock_get):
+def test_get_data_from_api_with_patch(mock_get):
     # Mocked JSON response
     mock_get.return_value.json.return_value = {
         "result_number": "10"
