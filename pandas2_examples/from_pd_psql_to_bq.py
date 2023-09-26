065a5d08bac193054055712988ffd359ccd526b5
Axel Furlan <axel.furlan@toptal.com>
2023-09-25 21:57:24 -0300

    more deps and more examples

--- pandas2_examples/from_pd_psql_to_bq.py
+++ pandas2_examples/from_pd_psql_to_bq.py
@@ -1,23 +1,21 @@
 import pandas as pd
-import psycopg2
 from sqlalchemy import create_engine
 
-# Create a connection using psycopg2
-conn = psycopg2.connect(
-    dbname="itba-dev",
-    host="localhost",
-    port="5432"
-)
+# create postgres engine
+engine = create_engine('postgresql://:@localhost:5432/itba-dev')
 
 # Transform a SQL query into a DataFrame using pandas
 sql = "SELECT * FROM titanic"
-df = pd.read_sql(sql, conn)
+df = pd.read_sql(sql, engine)
 
-
-# Create the BigQuery engine
-engine = create_engine(
-    'bigquery://itoa-dev-2/itba-dev'
-)
 
 # Write the DataFrame to a BigQuery table
-df.to_sql('titanic_2', engine)
+# this is very slow:
+# Create the BigQuery engine
+# engine = create_engine(
+#     'bigquery://itoa-dev-2/itba_dev'
+# )
+# df.to_sql('titanic_2', engine, index=False, if_exists='replace')
+
+# this is faster:
+df.to_gbq('itba_dev.titanic_2', project_id='itoa-dev-2', if_exists='replace')
